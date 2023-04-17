d24c3c15b4a023d3ca1d33700588da22df5dbf6d
kimitawanjohi <kimitawanjohi7923@gmail.com>
2023-04-17 15:37:00 +0100

    Fix linters issue

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const ART_API = 'https://collectionapi.metmuseum.org/public/collection/v1/objects'
-export const INVOLVEMENT_API = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/'
+export const ART_API = 'https://collectionapi.metmuseum.org/public/collection/v1/objects';
+export const INVOLVEMENT_API = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/';
