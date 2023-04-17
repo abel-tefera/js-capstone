a91c7442998434d9a02461ab3346690b340272cc
kimitawanjohi <kimitawanjohi7923@gmail.com>
2023-04-17 15:36:48 +0100

    Add hotreload to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -13,6 +13,16 @@ module.exports = {
       template: path.resolve(__dirname, 'src', 'index.html'),
     }),
   ],
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'src'),
+    },
+    port: 8080,
+    open: true,
+    hot: true,
+    compress: true,
+    historyApiFallback: true,
+  },
   module: {
     rules: [
       {
