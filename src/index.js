75d9799c8d338a0b8d66bfe02220f5a8fcaf4c42
kimitawanjohi <kimitawanjohi7923@gmail.com>
2023-04-18 19:18:50 +0100

    build pop up

--- src/index.js
+++ src/index.js
@@ -1,2 +1,26 @@
 import './style.css';
 import './tailwind.css';
+
+
+// modal 
+
+const modal = document.querySelector('#modal');
+const closeButton = document.querySelector('#close-modal');
+const openButton = document.querySelector('#open-modal');
+
+const modalClose = () => {
+  modal.classList.remove('fadeIn');
+  modal.classList.add('fadeOut');
+  setTimeout(() => {
+    modal.style.display = 'none';
+  }, 500);
+}
+
+const openModal = () => {
+  modal.classList.remove('fadeOut');
+  modal.classList.add('fadeIn');
+  modal.style.display = 'flex';
+}
+
+closeButton.addEventListener('click', modalClose);
+openButton.addEventListener('click', openModal);
