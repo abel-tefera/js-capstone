46188db1539c2192192c59f2d17cc0aedfc2b800
kimitawanjohi <kimitawanjohi7923@gmail.com>
2023-04-18 18:32:35 +0100

    Link html with bundle.sj

--- src/index.js
+++ src/index.js
@@ -1,6 +1,2 @@
 import './style.css';
 import './tailwind.css';
-// import logo from './assets/logo.jpg';
-
-// const img = document.querySelector('brand');
-// img.src = mainImage;
--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,7 @@ module.exports = {
   entry: {index: path.resolve(__dirname, 'src', 'index.js')},
   output: {
     path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
   },
   plugins: [
     new HtmlWebpackPlugin({
