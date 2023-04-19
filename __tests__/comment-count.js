a25eb873b1681a781b88b88091b418e0473889eb
kimitawanjohi <kimitawanjohi7923@gmail.com>
2023-04-19 19:02:40 +0100

    building tests

--- __tests__/comment-count.js
+++ __tests__/comment-count.js
@@ -1,9 +1,59 @@
-import commentCount from '../comment-count';
+import commentCounter from '../src/module/comment-counter.js';
 
-describe('test comment count', () => {
-    it('should return 2', () => {
-        const result = commentCount('/* eslint-disable */');
-        expect(result).toBe(2);
+const comments = document.createElement('div');
+comments.innerHTML = `
+<h4 class='text-gray-900 text-xl font-semibold'>Comments</h4>
+<ul id='comments-list' class='border border-gray-300 p-4 rounded-md shadow-sm mt-2'>
+
+</ul>`;
+const commentsList = comments.querySelector('#comments-list');
+
+const commentsArray = [
+        {
+            username: 'test',
+            comment: 'test comment',
+            creation_date: '2021-09-01',
+        },
+        {
+            username: 'test',
+            comment: 'test comment',
+            creation_date: '2021-09-01',
+        },
+]
+
+// mock fetch comments
+
+let getComments = () => commentsArray;
+
+// mock post comment
+
+let postComment = () => commentsArray.push({
+    username: 'test',
+    comment: 'test comment',
+    creation_date: '2021-09-01',
+})
+
+function appendComments(comments) {
+    comments.forEach((comment) => {
+        const li = document.createElement('li');
+        li.classList.add('flex', 'flex-col', 'py-2', 'border-b', 'border-gray-300');
+        li.innerHTML = `<div class='flex justify-between items-center'>
+            <p class='font-bold'>${comment.username}</p>
+            <p class='text-sm text-gray-500'>${comment.creation_date}</p>
+        </div>
+        <p class='text-sm'>${comment.comment}</p>`;
+        commentsList.appendChild(li);
     });
-    }
-);
+}
+
+// get comments
+describe('test comment count', () => {
+    test('should return 2', () => {
+        let comments = getComments();
+        expect(comments.length).toBe(2);
+        appendComments(comments);
+        expect(commentsList.children.length).toBe(2);
+        postComment();
+        expect(commentsList.children.length).toBe(3);
+    });
+});
