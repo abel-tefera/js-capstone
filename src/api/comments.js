0aaf1857afb39ee8f5c9c09ed765cf52dad5df31
kimitawanjohi <kimitawanjohi7923@gmail.com>
2023-04-19 11:33:05 +0100

    fix linters

--- src/api/comments.js
+++ src/api/comments.js
@@ -1,7 +1,9 @@
 import { INVOLVEMENT_API, APP_ID } from '../constants.js';
 
 export const getComments = async (id) => {
-  const response = await fetch(`${INVOLVEMENT_API}apps/${APP_ID}/comments?item_id=${id}`);
+  const response = await fetch(
+    `${INVOLVEMENT_API}apps/${APP_ID}/comments?item_id=${id}`,
+  );
   const data = await response.json();
   return data;
 };
--- src/views/modal.js
+++ src/views/modal.js
@@ -9,11 +9,11 @@ const createModal = (item) => {
     comments.forEach((comment) => {
       const li = document.createElement('li');
       li.classList.add('flex', 'flex-col', 'py-2');
-      li.innerHTML = `<div class="flex justify-between items-center">
-        <p class="font-bold">${comment.username}</p>
-        <p class="text-sm text-gray-500">${comment.creation_date}</p>
+      li.innerHTML = `<div class='flex justify-between items-center'>
+        <p class='font-bold'>${comment.username}</p>
+        <p class='text-sm text-gray-500'>${comment.creation_date}</p>
       </div>
-      <p class="text-sm">${comment.comment}</p>`;
+      <p class='text-sm'>${comment.comment}</p>`;
 
       commentsList.appendChild(li);
     });
@@ -26,70 +26,70 @@ const createModal = (item) => {
   modal.classList.add(...modalClasses.split(' '));
   const modalContent = document.createElement('div');
   modalContent.classList.add('py-4', 'px-6', 'text-left');
-  modalContent.innerHTML = `<div class="flex justify-between items-center pb-3">
-    <p class="text-2xl font-bold">${item.title}</p>
-    <div id="close-modal" class="modal-close cursor-pointer z-50">
+  modalContent.innerHTML = `<div class='flex justify-between items-center pb-3'>
+    <p class='text-2xl font-bold'>${item.title}</p>
+    <div id='close-modal' class='modal-close cursor-pointer z-50'>
       <svg
-        class="fill-current text-black"
-        xmlns="http://www.w3.org/2000/svg"
-        width="18"
-        height="18"
-        viewBox="0 0 18 18"
+        class='fill-current text-black'
+        xmlns='http://www.w3.org/2000/svg'
+        width='18'
+        height='18'
+        viewBox='0 0 18 18'
       >
         <path
-          d="M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z"
+          d='M14.53 4.53l-1.06-1.06L9 7.94 4.53 3.47 3.47 4.53 7.94 9l-4.47 4.47 1.06 1.06L9 10.06l4.47 4.47 1.06-1.06L10.06 9z'
         ></path>
       </svg>
     </div>
   </div>
   <!--Body-->
-  <div class="my-5">
+  <div class='my-5'>
     <div>
       <img
-        src="${item.img}"
-        class="h-auto md:h-60 lg:-h-96 w-full object-center mx-auto"
-        alt="image"
+        src='${item.img}'
+        class='h-auto md:h-60 lg:-h-96 w-full object-center mx-auto'
+        alt='image'
       />
     </div>
-    <div class="mt-4">
+    <div class='mt-4'>
       <!-- comments list -->
-      <h4 class="text-gray-900 text-xl font-semibold">Comments</h4>
-      <ul id="comments-list" class="mt-2 max-h-40 overflow-y-scroll">
+      <h4 class='text-gray-900 text-xl font-semibold'>Comments</h4>
+      <ul id='comments-list' class='mt-2 max-h-40 overflow-y-scroll'>
 
       </ul>
-      <form id="form-${item.id}" class="mt-4 shadow-sm">
-        <h4 class="text-gray-900 text-xl font-semibold">Add Comment</h4>
-        <div class="flex flex-col">
+      <form id='form-${item.id}' class='mt-4 shadow-sm'>
+        <h4 class='text-gray-900 text-xl font-semibold'>Add Comment</h4>
+        <div class='flex flex-col'>
           <label
-            for="name"
-            class="text-gray-700 font-semibold mb-1"
+            for='name'
+            class='text-gray-700 font-semibold mb-1'
           >
             Name
           </label>
           <input
-            id="name"
-            name="name"
-            type="text"
-            class="border border-gray-300 p-2 rounded"
+            id='name'
+            name='name'
+            type='text'
+            class='border border-gray-300 p-2 rounded'
           />
 
           <label
-            for="comment"
-            class="text-gray-700 mt-2 font-semibold mb-1"
+            for='comment'
+            class='text-gray-700 mt-2 font-semibold mb-1'
           >
             Comment
           </label>
           <textarea
-            id="comment"
-            name="comment"
-            class="border border-gray-300 p-2 rounded resize-none"
-            rows="2"
+            id='comment'
+            name='comment'
+            class='border border-gray-300 p-2 rounded resize-none'
+            rows='2'
           ></textarea>
         </div>
-        <div class="flex justify-end mt-2">
+        <div class='flex justify-end mt-2'>
           <button
-            id="comment-btn"
-            class="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+            id='comment-btn'
+            class='bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600'
           >
             Comment
           </button>
